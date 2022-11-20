2cbfb9abb9b1cce42592d1b7bd5aa7f7f1096c04
Charles Lechasseur <shiftingbeard@gmx.com>
2022-11-19 19:21:35 -0500

    Day 1

--- src/main/kotlin/io/github/clechasseur/adventofcode/y2015/Day1.kt
+++ src/main/kotlin/io/github/clechasseur/adventofcode/y2015/Day1.kt
@@ -1,5 +1,19 @@
 package io.github.clechasseur.adventofcode.y2015
 
+import io.github.clechasseur.adventofcode.y2015.data.Day1Data
+
 object Day1 {
-    fun part1(): Int = 0
+    private val input = Day1Data.input
+
+    fun part1(): Int = input.mapToFloorMoves().sum()
+
+    fun part2(): Int = input.mapToFloorMoves().foldIndexed(0 to 0) { idx, (cidx, sum), i ->
+        (if (sum == -1) cidx else idx + 1) to (if (sum == -1) -1 else sum + i)
+    }.first
+
+    private fun String.mapToFloorMoves(): List<Int> = map { when (it) {
+        '(' -> 1
+        ')' -> -1
+        else -> error("Wrong input char: $it")
+    } }
 }
--- src/test/kotlin/io/github/clechasseur/adventofcode/y2015/AdventOfCode2015.kt
+++ src/test/kotlin/io/github/clechasseur/adventofcode/y2015/AdventOfCode2015.kt
@@ -7,7 +7,12 @@ class AdventOfCode2015 {
     class Day1Puzzles {
         @Test
         fun `day 1, part 1`() {
-            assertEquals(0, Day1.part1())
+            assertEquals(138, Day1.part1())
+        }
+
+        @Test
+        fun `day 1, part 2`() {
+            assertEquals(0, Day1.part2())
         }
     }
 }
